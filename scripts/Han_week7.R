c58d4a706a4002197f6f2de291d3c224055f9c3f
itsjameshan <chengchouhan@huskers.unl.edu>
2016-02-23 11:28:54 -0600

    Change the folder name from R script to scripts.

---
+++ scripts/Han_week7.R
@@ -0,0 +1,7 @@
+# Week 7 moRe R, James Han 2-23-2016
+getwd()
+gapminder <- read.table("data/gapminder-FiveYearData.csv",
+                     header=TRUE,
+                     sep=",")
+gapminder
+ 
